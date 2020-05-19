21c852e3d77cd6405ea57809b3a3fc8ece43029b
sudhanshu150 <42195119+sudhanshu150@users.noreply.github.com>
2020-05-19 23:06:44 +0530

    Add files via upload

---
+++ werid algorithum.cpp
@@ -0,0 +1,43 @@
+/*Consider an algorithm that takes as input a positive integer n. If n is even, the algorithm divides it by two, and if n is odd, the algorithm multiplies it by three and adds one. The algorithm repeats this, until n is one. For example, the sequence for n=3 is as follows:
+3→10→5→16→8→4→2→1
+
+Your task is to simulate the execution of the algorithm for a given value of n.
+
+Input
+
+The only input line contains an integer n.
+
+Output
+
+Print a line that contains all values of n during the algorithm.
+
+Constraints
+1≤n≤106
+Example
+
+Input:
+3
+
+Output:
+3 10 5 16 8 4 2 1*/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    for(int i=0;i<n+10;i++)
+    {
+        if(n==1)
+            break;
+        else if(n%2==0)
+            n=n/2;
+        else
+            n=(n*3)+1;
+    cout<<n<<" ";
+    }
+
+    return 0;
+}
